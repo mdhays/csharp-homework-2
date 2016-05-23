7b14ab7e5885f76e1f2330502c217f5200d243b6
Matthew <mdhays1995@gmail.com>
2016-05-22 21:02:34 -0500

    Reverses the .txt file and outputs the results to the console.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("../../example.txt");
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            string output = new string(inputArray);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
